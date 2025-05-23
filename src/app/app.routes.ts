687ec10bdeb9d4ee5e88a6769e05297ed0a7cb7a
PC-RICARDO01\Claudio Batta <ngonzalez@pegasoconsulting.net>
2025-05-23 15:54:35 -0400

    recuperacion de clave exitoso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component'; // <-- ¡Importar el RegisterComponent!
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
@@ -11,7 +12,13 @@ export const routes: Routes = [
   { path: 'auth/register', component: RegisterComponent }, // <-- ¡Añadir la ruta de registro!
   {
     path: 'auth/forgot-password',
-    loadComponent: () => import('./auth/forgot-password/forgot-password.component').then(m => ForgotPasswordComponent)
+    loadComponent: () =>
+      import('./auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
+  },
+  {
+    path: 'auth/reset-password',
+    loadComponent: () =>
+      import('./auth/reset-password/reset-password.component').then(m => m.ResetPasswordComponent)
   },
   // ... otras rutas
   { path: '**', redirectTo: '' }
