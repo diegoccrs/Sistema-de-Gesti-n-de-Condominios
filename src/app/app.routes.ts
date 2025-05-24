f204da26614f37d76070aad911a54bfd3ce48375
GianfrancoMongiell0 <gianfranco.mongiello@correo.unimet.edu.ve>
2025-05-24 17:49:58 -0400

    feat: arreglo del forgot-password y reset-password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
-
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component'; // Añadir
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component'; // Añadir
 import { authGuard } from './auth/guards/auth.guard';
 import { UnauthorizedComponent } from './auth/unauthorized/unauthorized.component';
 
@@ -10,6 +12,8 @@ export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
+  { path: 'auth/forgot-password', component: ForgotPasswordComponent }, // Nueva ruta
+  { path: 'auth/reset-password', component: ResetPasswordComponent }, // Nueva ruta
   { path: 'unauthorized', component: UnauthorizedComponent },
   {
     path: 'dashboard',
