701c12171fa3c0e4406156c0240c648ee39c533a
PC-RICARDO01\Claudio Batta <ngonzalez@pegasoconsulting.net>
2025-06-12 15:34:36 -0400

    estilo incorporado

--- src/app/dashboard/components/resident-dashboard/payment-method-dialog/payment-method-dialog.component.ts
+++ src/app/dashboard/components/resident-dashboard/payment-method-dialog/payment-method-dialog.component.ts
@@ -3,6 +3,8 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-payment-method-dialog',
@@ -12,17 +14,22 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     CommonModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
   ],
 })
 
 export class PaymentMethodDialogComponent {
+  payment: any; // ✅ define la propiedad
+
   constructor(
     public dialogRef: MatDialogRef<PaymentMethodDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+    @Inject(MAT_DIALOG_DATA) public data: any // ← datos que vienen del componente padre
+  ) {
+    this.payment = data; // ✅ así puedes usar `payment` en el HTML
+  }
 
   selectMethod(method: string) {
-    this.dialogRef.close(method);
+    this.dialogRef.close({ selectedMethod: method });
   }
 }
