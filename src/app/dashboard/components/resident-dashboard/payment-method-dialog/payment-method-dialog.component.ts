f5db1840eac76bf6e0e662d6da692fd69256429d
santilanzb <santilanzb@gmail.com>
2025-06-15 23:22:32 -0400

    Currency VES not permitted handled

--- src/app/dashboard/components/resident-dashboard/payment-method-dialog/payment-method-dialog.component.ts
+++ src/app/dashboard/components/resident-dashboard/payment-method-dialog/payment-method-dialog.component.ts
@@ -1,3 +1,4 @@
+// src/app/dashboard/components/resident-dashboard/payment-method-dialog/payment-method-dialog.component.ts
 import { Component, Inject, AfterViewInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
@@ -5,7 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 
-declare var paypal: any; // Declare the PayPal global object
+declare var paypal: any;
 
 @Component({
   selector: 'app-payment-method-dialog',
@@ -21,26 +22,36 @@ declare var paypal: any; // Declare the PayPal global object
 })
 export class PaymentMethodDialogComponent implements AfterViewInit {
   payment: any;
+  isPayPalSupported: boolean = true; // Flag to check if PayPal is supported
 
   constructor(
     public dialogRef: MatDialogRef<PaymentMethodDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     this.payment = data;
+    // PayPal does not support VES, so we disable it for that currency
+    if (this.payment.currency === 'VES') {
+      this.isPayPalSupported = false;
+    }
   }
 
   ngAfterViewInit(): void {
-    this.renderPayPalButton();
+    if (this.isPayPalSupported) {
+      this.renderPayPalButton();
+    }
   }
 
   renderPayPalButton(): void {
+    
+    console.log('Payment data for PayPal:', this.payment);
+
     paypal.Buttons({
       createOrder: (data: any, actions: any) => {
         return actions.order.create({
           purchase_units: [{
             amount: {
-              value: this.payment.amount.toString(),
-              currency_code: this.payment.currency
+              value: Number(this.payment.amount).toFixed(2),
+              currency_code: this.payment.currency || 'USD'
             },
             description: this.payment.concept
           }]
